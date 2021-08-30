5f5fad4174c92348b0889e2279d18983f4a47eaa
Salomi <salomisindhu.33@gmail.com>
2021-08-29 23:12:44 -0400

    add filter to completed and incomplete. finished commit

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 
 //Event listeners when button is clicked
 
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 
 //Functions
@@ -55,11 +57,51 @@ function deleteCheck(e) {
     //grab the item user clicks
     const item = e.target;
 
-    //delete the item
+    //delete button removes the item
     if (item.classList[0] === "trash-btn") {
-        const todo = item.parentElement
-        todo.remove();
+        const todo = item.parentElement;
+
+        //Animation
+        todo.classList.add("fall");
+
+        todo.addEventListener('transitionend', function () {
+            todo.remove();
+        });
+        //todo.remove();
+    }
+
+    //check mark makes item completed
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
     }
 
 
+}
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function (todo) {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+
+        }
+
+    });
 }
